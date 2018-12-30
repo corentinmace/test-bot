834903633651246bbc68aac19857cbc4fa9bbd35
Corentin MACE <corentin.mace@ynov.com>
2018-12-30 14:29:45 +0100

    Adding "fire" command

--- bot.js
+++ bot.js
@@ -9,10 +9,12 @@ bottoken = config.bottoken;
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+ client.user.setPresence({ game: { name: `Il y a actuellement ${client.guilds.get(servtoken).memberCount} étudiants !`, type: 0 } });
+
 });
 
 client.on('message', message => {
-  if (message.content === '<@512176143072231434>') {
+  if (message.content === '<@528892444444459009>') {
     message.reply( '.help si tu as besoin d\'aide');
   }
   if(message.content === prefix + "info"){
@@ -98,29 +100,43 @@ client.on('message', message => {
      snx.send(`${message.author} veut rejoindre le snx gang !`);
    }
 
-   if (message.content.startsWith(prefix + "accept") && message.member.roles.has("528573193510846465")) {
-     member.addRole("528573193510846465");
+   if (message.content.startsWith(prefix + "accept") && message.member.roles.has("528573193510846465") && message.member.roles.has("528892097668055042")) {//Remplacer par le rôle de la WFU et Leader de clan
+     member.addRole("528573193510846465"); //Remplacer par le rôle de la Waifu Army
 
      message.reply(`Bienvenue dans la Waifu Army ${user.tag}`);
 
    }
-   if (message.content.startsWith(prefix + "refuse") && message.member.roles.has("528573193510846465")) {
+   if (message.content.startsWith(prefix + "fire") && message.member.roles.has("528573193510846465") && message.member.roles.has("528892097668055042")) {//Remplacer par le rôle de la WFU et Leader de clan
+     member.removeRole("528573193510846465"); //Remplacer par le rôle de la Waifu Army
+
+     message.reply(`${user.tag} à été viré du clan !`);
+
+   }
+   if (message.content.startsWith(prefix + "refuse") && message.member.roles.has("528573193510846465") && message.member.roles.has("528892097668055042")) {//Remplacer par le rôle de la WFU et Leader de clan
      member.send("Désolé mais tu n'as pas été accepté à la Waifu Army");
    }
 
-   if (message.content.startsWith(prefix + "accept") && message.member.roles.has("528573281268137984")) {
-     member.addRole("528573281268137984");
-     message.reply(`Bienvenue dans l'Arcane de l'Umbra @${user.tag}`);
+   if (message.content.startsWith(prefix + "accept") && message.member.roles.has("528573281268137984") && message.member.roles.has("528892097668055042")) { //Remplacer par le rôle de la AU et Leader de clan
+     member.addRole("528573281268137984"); //Remplacer par le rôle de AU
+     message.reply(`Bienvenue dans l'Arcane de l'Umbra ${user.tag}`);
    }
-   if (message.content.startsWith(prefix + "refuse") && message.member.roles.has("528573281268137984")) {
+   if (message.content.startsWith(prefix + "fire") && message.member.roles.has("528573281268137984") && message.member.roles.has("528892097668055042")) { //Remplacer par le rôle de la AU et Leader de clan
+     member.removeRole("528573281268137984"); //Remplacer par le rôle de AU
+     message.reply(`${user.tag} à été viré du clan`);
+   }
+   if (message.content.startsWith(prefix + "refuse") && message.member.roles.has("528573281268137984") && message.member.roles.has("528892097668055042")) { //Remplacer par le rôle de la WFU et Leader de clan
      member.send("Désolé mais tu n'as pas été accepté à l'Arcane de l'Umbra");
    }
 
-   if (message.content.startsWith(prefix + "accept") && message.member.roles.has("528573233528700948")) {
-     member.addRole("528573233528700948");
-     message.reply(`Bienvenue dans le snx gang @${user.tag}`);
+   if (message.content.startsWith(prefix + "accept") && message.member.roles.has("528573233528700948") && message.member.roles.has("528892097668055042")) { //Remplacer par le rôle de snx et Leader de clan
+     member.addRole("528573233528700948"); //Remplacer par le role de SNX
+     message.reply(`Bienvenue dans le snx gang ${user.tag}`);
    }
-   if (message.content.startsWith(prefix + "refuse") && message.member.roles.has("528573233528700948")) {
+   if (message.content.startsWith(prefix + "fire") && message.member.roles.has("528573233528700948") && message.member.roles.has("528892097668055042")) { //Remplacer par le rôle de snx et Leader de clan
+     member.removeRole("528573233528700948"); //Remplacer par le role de SNX
+     message.reply(`${user.tag} à été viré du clan`);
+   }
+   if (message.content.startsWith(prefix + "refuse") && message.member.roles.has("528573233528700948")&& message.member.roles.has("528892097668055042")) { //Remplacer par le rôle de snx et Leader de clan
      member.send("Désolé mais tu n'as pas été accepté au snx gang");
    }
 
